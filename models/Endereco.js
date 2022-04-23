abaff5a85555d30644f0a4c43c9d71155c66d676
Farley Junio <ifarley_xd@hotmail.com>
2022-04-23 19:55:36 -0300

    Tabela Endereco.js criada

---
+++ models/Endereco.js
@@ -0,0 +1,13 @@
+const Endereco = (sequelize, DataTypes) => {
+    const endereco = sequelize.define('Endereco', {
+        rua: DataTypes.STRING,
+        cidade: DataTypes.STRING,
+        estado: DataTypes.STRING,
+        telefone: DataTypes.STRING,
+        usuario_id: DataTypes.INTEGER,
+    },{
+        tableName: "endereco",
+        timestamps: false
+    })
+    return endereco
+}
