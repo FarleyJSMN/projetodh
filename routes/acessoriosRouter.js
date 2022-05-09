bbbf6d0748e979cb7b29a6a4369df9fad7ccc0f8
Farley Junio <ifarley_xd@hotmail.com>
2022-05-09 19:08:28 -0300

    Routers removidos

--- routes/acessoriosRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router()
-const acessoriosController = require('../controller/acessoriosController')
-
-router.get('/acessorios', acessoriosController.listaAcessorios)
-
-module.exports = router
--- routes/pecasRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const pecasController = require('../controller/pecasController')
-
-router.get('/pecas', pecasController.listaPecas)
-
-module.exports = router
