8983c4a7a1855779c2a1bb7e14cb118894b81d85
Farley Junio <ifarley_xd@hotmail.com>
2022-05-09 18:27:38 -0300

    Controller criar usuario

---
+++ controller/Usuario/criarUsuario.js
@@ -0,0 +1,27 @@
+const { sequelize, Usuario } = require('../../models/index');
+const bcrypt = require('bcrypt');
+
+const telaCadastro = async (req, res) => {
+    res.render('usuario/cadastro-form')
+} 
+
+const criarUsuario = async (req, res) => {
+    const {nome, sobrenome, email, senha} = req.body
+    const hash = await bcrypt.hash(senha, 10)
+    const criandoUsuario = await Usuario.create({
+        nome: nome,
+        sobrenome: sobrenome,
+        email: email,
+        senha: hash,
+    })
+
+    try {
+        await criandoUsuario.save()
+        res.status(200).render('usuario/login-form')
+
+    } catch (error) {
+        res.status(500).send(error)
+    }
+}
+
+module.exports = {criarUsuario, telaCadastro}
