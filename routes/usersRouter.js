43456975746eefd83ba00aaac5529227163c1571
Farley Junio <ifarley_xd@hotmail.com>
2022-05-19 11:25:21 -0300

    Rota perfil criada, middleware de validacao de usuario(validaUsuario) importada

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 const usuario = require('../controller/Usuario/criarUsuario')
 const cadastro = require('../controller/Usuario/criarUsuario')
 const login = require('../controller/Usuario/criarLogin')
-const validaCadastro = require('../middlewares/Usuario/validacaoCadastro')
+const perfil = require('../controller/Usuario/editarUsuario')
+const validaUsuario = require('../middlewares/Usuario/validaUsuario')
 
 router.get('/cadastro', cadastro.telaCadastro)
 router.post('/cadastro', usuario.criarUsuario)
@@ -11,4 +12,6 @@ router.post('/cadastro', usuario.criarUsuario)
 router.get('/login', login.telaLogin)
 router.post('/login', login.criarLogin)
 
-module.exports = router;
+router.get('/perfil', validaUsuario, perfil.telaPerfil)
+
+module.exports = router
