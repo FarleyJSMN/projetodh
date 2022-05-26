274bf87ad7adfe62f6002a209ada2a63a9bf86b6
Farley Junio <ifarley_xd@hotmail.com>
2022-05-26 17:43:02 -0300

    Mensagem de validacao do login para o usuario

--- controller/Usuario/criarLogin.js
+++ controller/Usuario/criarLogin.js
@@ -2,7 +2,7 @@ const { sequelize, Usuario } = require('../../models/index');
 const bcrypt = require('bcrypt');
 
 const telaLogin = async (req, res) => {
-    res.render('usuario/login-form')
+    res.render('usuario/login-form', { message: ''})
 }
 
 const criarLogin = async (req, res) => {
@@ -10,12 +10,12 @@ const criarLogin = async (req, res) => {
     const buscarEmail = await Usuario.findOne({ where: { email: email } })
 
     if (!buscarEmail) {
-        res.render('usuario/login-form')
+        res.render('usuario/login-form', { message: 'Email não cadastrado'})
     } else {
         const senhaCriptada = await bcrypt.compare(senha, buscarEmail.senha)
 
         if(!senhaCriptada){
-        res.render('usuario/login-form')
+        res.render('usuario/login-form', { message: 'Senha incorreta'})
         }
 
         if (senhaCriptada && email == buscarEmail.email) {
