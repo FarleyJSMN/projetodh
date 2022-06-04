6b8266f83939b42e081f2f3af2bcf3b5d0c945be
Gabrielle Coelho Sanfilippo <gabriellecoelhosanfilippo@gmail.com>
2022-06-04 17:57:30 -0300

    logout

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -4,5 +4,6 @@ const indexController = require('../controller/indexController')
 
 /* GET home page. */
 router.get('/', indexController.index);
+router.get('/logout', indexController.logout)
 
 module.exports = router;
